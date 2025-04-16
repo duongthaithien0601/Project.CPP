7f4a6382ebe1a123c95425bb946400833460f30b
duongthaithien0601 <benjaminduong6126@gmail.com>
2025-04-16 15:04:32 +0700

    push code chinh len

--- Baitaplon.cpp
+++ Baitaplon.cpp
@@ -1,2 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+// Ty le quy doi
+const double DOLLARS_PER_POINT = 10.0; // 10 do = 1 diem
+
+// Lop Vi
+class Wallet {
+public:
+    int walletId;
+    int balance;
+    vector<string> transactionLog;
+
+    Wallet() : walletId(-1), balance(0) {}
+    Wallet(int id, int initialBalance = 0) : walletId(id), balance(initialBalance) {}
+};
